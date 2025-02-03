8d58606ec5ea0cab6c2025b5387c14a3f4931d0d
weerapat-p <weerapat.p@allianz.co.th>
2025-02-03 21:42:45 +0700

    add img

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [TabMenuModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  items:any[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+        { label: 'หน้าแรก'},
+        { label: 'สิ้นค้าของเรา'},
+        { label: 'วิธีสั่งซื้อ'},
+        { label: 'ติดต่อเรา' }
+    ]
+}
 
 }
