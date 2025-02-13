9fe3b8d7916cf835ec6e2294fd85b108f03e57fd
petchzahahayo <petchzahahayo1@gmail.com>
2025-02-13 23:44:30 +0700

    add component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductComponent } from './pages/product/product.component';
 
 export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+  },
+  {
+    path: 'product',
+    component: ProductComponent,
   },
   {
     path: '**',
