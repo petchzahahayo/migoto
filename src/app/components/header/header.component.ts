74303c0ffbd3687322666e85a969cfe120c82629
petchzahahayo <petchzahahayo1@gmail.com>
2025-02-22 00:06:49 +0700

    add sidebar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
-  constructor(
- 
-    private router: Router
-  ) {}
+  sidebarVisible: boolean = false;
+  constructor(private router: Router) {}
 
   goto(path: string) {
     // Navigate silently to /view
